f120e897cb289b2c1b4677353e19844cd20de3cb
Neha Pramanik <nehapramanik50@gmail.com>
2021-08-27 20:15:58 +0530

    Saving into local storage

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var elements = [];
 
+//Loading previous tasks
+window.onload=function(){
+    if(JSON.parse(localStorage.getItem("Tasks"))!=null){
+        elements=JSON.parse(localStorage.getItem("Tasks"));
+        display();
+    }
+}
+
 //Adding new tasks
 function addNewTask(){
     if(document.querySelector(".addText").value.trim()!=""){
@@ -9,12 +17,13 @@ function addNewTask(){
     }
 } 
 
-//Displaying updates
+//Displaying updates and saving into local storage
 function display(){
     document.querySelector(".list").innerHTML="";
     for(var i=0; i<elements.length; i++){
         document.querySelector(".list").innerHTML += "<div class='element'>" + elements[i] + "<img class='tickIcon' src='done.png' onclick='tick("+i+")'> <img class='dustbin' src='dustbin_icon.png' onclick='del("+i+")'></div>";
     }
+    localStorage.setItem("Tasks", JSON.stringify(elements));
 }
 
 //Deleting task
@@ -32,4 +41,3 @@ function tick(index){
     else elements[index]="<strike>" + elements[index] + "</strike>";
     display();
 }
-
